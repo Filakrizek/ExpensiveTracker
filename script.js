ff0edf67109a953febc176f1e79ba34cb5bec693
Filakrizek <104352667+Filakrizek@users.noreply.github.com>
2023-08-03 14:47:45 +0000

    Js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var price = 0
-var things = ''
+var things = document.getElementById('thing').value
+var price  =  document.getElementById('price').value
+var numberofthings = document.getElementById('numberOfThings').value 
+var date = document.getElementById('date').value 
+function main(){
+document.getElementById('result').value = things + ': ' + price  + ': ' + numberofthings + ': ' + date  + ' ' ;
+  }
